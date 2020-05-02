0acbd922aa752ae8b099ac75a8f6c26ab416e6c8
Ricardo Santos <ricardo.eletris@hotmail.com>
2020-05-01 21:02:13 -0300

    update arquivos de herança 1 e 2 de 04 objeto

--- 04 objeto/08 heranca1.js
+++ 04 objeto/08 heranca1.js
@@ -8,12 +8,12 @@ const volvo = {
     velMax: 200
 }
 
-console.log(ferrari.prototype)
-console.log(ferrari.__proto__)
-console.log(ferrari.__proto__ === Object.prototype)
-console.log(volvo.__proto__ === Object.prototype)
+console.log(ferrari.prototype)  // objeto ferrari nãon tem o atributo prototype
+console.log(ferrari.__proto__)    // __proto ira procurar em todos os prototipos ou pais
+console.log(ferrari.__proto__ === Object.prototype)  // é verdadeiro onde proto é igual esta dentro de object
+console.log(volvo.__proto__ === Object.prototype) // 
 console.log(Object.prototype.__proto__ === null)
 
 function MeuObjeto() {}
 console.log(typeof Object, typeof MeuObjeto)
-console.log(Object.prototype, MeuObjeto.prototype)
+console.log(Object.prototype, MeuObjeto.prototype)  //Object.prototype é um objeto vazio    e MeuObjeto.prototype é uma função
--- 04 objeto/09 heranca2.js
+++ 04 objeto/09 heranca2.js
@@ -1,46 +1,46 @@
 // Cadeia de protótipos (prototype chain)
-Object.prototype.attr0 = '0' // não faça isso em casa!
+Object.prototype.attr0 = '0' // não faça isso em casa!  
 
-const avo = { attr1: 'A' }
-const pai = { __proto__: avo, attr2: 'B', attr3: '3' }
-const filho = { __proto__: pai, attr3: 'C' }
-console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
+const avo = { attr1: 'A' }  // tera somente o Object.prototype
+const pai = { __proto__: avo, attr2: 'B', attr3: '3' }  // para falar que avo é pai inclui __proto__: avo em seguinda os demais atributos
+const filho = { __proto__: pai, attr3: 'C' }   // no attr 3 = C vai se sobresair o do pai que é 3
+console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3) // no caso ele vai procurar nos demais pais o atributo solicitado no attr0 ele vai pegar do avo onde foi que object.prototype = 0 
 
-const carro = {
+const carro = {   // cria o obj carro que será o prototipo
     velAtual: 0,
     velMax: 200,
-    acelerarMais(delta) {
-        if (this.velAtual + delta <= this.velMax) {
-            this.velAtual += delta
+    acelerarMais(delta) {   // delta que é a diferença que quer acelerar
+        if (this.velAtual + delta <= this.velMax) {   // se a velocidade atual + delta for menor = que velmax
+            this.velAtual += delta    // vai incluir o valor de velatual em delta
         } else {
-            this.velAtual = this.velMax
+            this.velAtual = this.velMax   //se não for menor ou igual vel max vai colocar delta como velMax
         }
     },
     status() {
-        return `${this.velAtual}Km/h de ${this.velMax}Km/h`
+        return `${this.velAtual}Km/h de ${this.velMax}Km/h`   //vai somente mostrar o valor atual do total que é velMax
     }
 }
 
 const ferrari = {
     modelo: 'F40',
-    velMax: 324 // shadowing
+    velMax: 324 // shadowing que vai sobrepor o valor que foi definido no obj carro
 }
 
 const volvo = {
     modelo: 'V40',
-    status() {
-        return `${this.modelo}: ${super.status()}`
+    status() {   // vai dar shadowing na função do obj carro
+        return `${this.modelo}: ${super.status()}`  // vai retornar somente o modelo desse obj e o super é para pegar a função status do obj carro que é o prototipo
     }
 }
 
-Object.setPrototypeOf(ferrari, carro)
+Object.setPrototypeOf(ferrari, carro)  // cria uma relação de prototypo entre 2 objetos ou seja ferrari tem como prototipo o carro
 Object.setPrototypeOf(volvo, carro)
 
-console.log(ferrari)
-console.log(volvo)
+console.log(ferrari)   // vai mostrar os atibutos de modelo e velmax
+console.log(volvo)    // vai mostrar modelo e função declarada que a status de obj carro
 
-volvo.acelerarMais(100)
-console.log(volvo.status())
+volvo.acelerarMais(100)   // ira chamar a função que esta em obj carro dando velocidad de 100
+console.log(volvo.status())   // informar o obj carro chamando a função status interna do obj volvo e status que é do obj carro
 
 ferrari.acelerarMais(300)
-console.log(ferrari.status())
+console.log(ferrari.status())    //pega normalmente a vel do obj ferrari
