41c4818e9e6b49bd4865a6b1f13323036341c749
Ricardo Santos <ricardo.eletris@hotmail.com>
2020-05-04 20:18:00 -0300

    Update 01 array.js

--- 05 array/01 array.js
+++ 05 array/01 array.js
@@ -1,30 +1,30 @@
 console.log(typeof Array, typeof new Array, typeof [])
 
-let aprovados = new Array('Bia', 'Carlos', 'Ana')
+let aprovados = new Array('Bia', 'Carlos', 'Ana')   //new função array que cria um objeto
 console.log(aprovados)
 
-aprovados = ['Bia', 'Carlos', 'Ana']
-console.log(aprovados[0])
+aprovados = ['Bia', 'Carlos', 'Ana']  // notação literal
+console.log(aprovados[0])   // busca pelo indice
 console.log(aprovados[1])
 console.log(aprovados[2])
 console.log(aprovados[3])
 
-aprovados[3] = 'Paulo'
-aprovados.push('Abia')
-console.log(aprovados.length)
+aprovados[3] = 'Paulo'   // adicção no indice informaddo
+aprovados.push('Abia')    // adiciona outro valor no array
+console.log(aprovados.length)   // quantidade de indices preenchidos
 
 aprovados[9] = 'Rafael'
 console.log(aprovados.length)
 console.log(aprovados[8] === undefined)
 
 console.log(aprovados)
-aprovados.sort()
+aprovados.sort()   // ordena o array
 console.log(aprovados)
 
-delete aprovados[1]
+delete aprovados[1]   //deleta o atributo mas não reordena os indices
 console.log(aprovados[1])
 console.log(aprovados[2])
 
 aprovados = ['Bia', 'Carlos', 'Ana']
-aprovados.splice(1, 1)
+aprovados.splice(1, 1)  // primeiro escolhe o indice segundo quantidade de excluidos caso queira adiconar colocar o segundo parametro com zero e colocar separdo por virgula em sequencio o que sera adicionado
 console.log(aprovados)
